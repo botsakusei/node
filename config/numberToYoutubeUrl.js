5603b296b2d02986f18b4ebd44cda088ec342d0e
botsakusei <kn2013kn03@gmail.com>
2025-09-20 00:12:22 +0900

    Update numberToYoutubeUrl.js

--- config/numberToYoutubeUrl.js
+++ config/numberToYoutubeUrl.js
@@ -1,6 +1,6 @@
 const numberToYoutubeUrl = {
   1: "https://www.youtube.com/watch?v=uOHI2d7c6uw&list=RDuOHI2d7c6uw&start_radio=1",
-  2: "https://www.youtube.com/watch?v=xxxxxxx2",
+  2: "https://www.youtube.com/watch?v=O42wSO-FEI8&list=RDO42wSO-FEI8&start_radio=1",
   3: "https://www.youtube.com/watch?v=xxxxxxx3",
   // 必要に応じて60番まで
   60: "https://www.youtube.com/watch?v=xxxxxxx60"
