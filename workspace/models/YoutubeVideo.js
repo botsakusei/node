22f97fe71b51d398955b28547bc389b9bb120535
botsakusei <kn2013kn03@gmail.com>
2025-09-19 23:27:31 +0900

    Create YoutubeVideo model with schema definition

---
+++ workspace/models/YoutubeVideo.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const YoutubeVideoSchema = new mongoose.Schema({
+  url: { type: String, required: true, unique: true },
+  owner: { type: String, default: '' },
+  count: { type: Number, default: 0 }
+});
+
+const YoutubeVideo = mongoose.model('YoutubeVideo', YoutubeVideoSchema);
+export default YoutubeVideo;
